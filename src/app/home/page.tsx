5b5c4d39b9033ae402b064429fa5080a86f16733
MarcosPaAc <isaelpa09@gmail.com>
2025-03-12 16:54:27 -0400

    Testing the routing feature

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+// app/page.js
+import React from "react";
+
+export default function Test() {
+    return (
+      <main>
+        <h1>Welcome to the Home Page</h1>
+      </main>
+    );
+  }
+  
